ebd9d39b57c12c4ef2452921774ae90a2361b054
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-01-19 15:44:45 +0400

    Add service structure and entities

---
+++ src/main/java/az/fitnest/paymentservice/entity/Payment.java
@@ -0,0 +1,33 @@
+package az.fitnest.paymentservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Long paymentId;
+    
+    @Column(name = "provider", nullable = false)
+    private String provider;
+    
+    @Column(name = "payment_method_id")
+    private String paymentMethodId;
+    
+    @Column(name = "payment_intent_client_secret")
+    private String paymentIntentClientSecret;
+    
+    @Column(name = "status", nullable = false)
+    private String status;
+}
