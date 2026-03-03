8ce15d3214c077e98449cae1a4e33a7be20203d7
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-03 10:34:50 +0400

    feat: add custom Jackson configuration for improved JSON serialization and deserialization

--- src/main/java/az/fitnest/payment/config/JacksonConfig.java
+++ src/main/java/az/fitnest/payment/config/JacksonConfig.java
@@ -8,20 +8,13 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
-        return new Jackson2ObjectMapperBuilder();
-    }
-
-    @Bean
     @Primary
-    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
 
         // Register Java 8 date/time module
         objectMapper.registerModule(new JavaTimeModule());
@@ -50,4 +43,5 @@ public class JacksonConfig {
         return objectMapper;
     }
 }
+
 
