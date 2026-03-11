78939258da15358de6cdc420f31c8742db9b0e59
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-11 11:39:37 +0400

    fix: add JsonProperty annotations to EpointCardRegistrationRequest fields for proper serialization

--- src/main/java/az/fitnest/payment/dto/epoint/EpointCardRegistrationRequest.java
+++ src/main/java/az/fitnest/payment/dto/epoint/EpointCardRegistrationRequest.java
@@ -1,19 +1,27 @@
 package az.fitnest.payment.dto.epoint;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record EpointCardRegistrationRequest(
     @Schema(hidden = true)
+    @JsonProperty("public_key")
     String publicKey,
+    @JsonProperty("language")
     String language,
+    @JsonProperty("refund")
     Integer refund,
+    @JsonProperty("description")
     String description,
     @Schema(hidden = true)
+    @JsonProperty("result_url")
     String resultUrl,
     @Schema(hidden = true)
+    @JsonProperty("success_redirect_url")
     String successRedirectUrl,
     @Schema(hidden = true)
+    @JsonProperty("error_redirect_url")
     String errorRedirectUrl
 ) {}
