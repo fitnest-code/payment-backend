99a0e4936e8e14c3e45f05f58d35202d7882d45e
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-31 12:20:27 +0400

    chore: add JsonProperty annotations to EpointExecutePayRequest fields for consistent JSON mapping

--- src/main/java/az/fitnest/payment/dto/epoint/EpointExecutePayRequest.java
+++ src/main/java/az/fitnest/payment/dto/epoint/EpointExecutePayRequest.java
@@ -1,23 +1,39 @@
 package az.fitnest.payment.dto.epoint;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
 @Builder
 public record EpointExecutePayRequest(
     @Schema(hidden = true)
+    @JsonProperty("public_key")
     String publicKey,
+
     String language,
+
+    @JsonProperty("order_id")
     String orderId,
+
     Double amount,
     String currency,
     String description,
+
     @Schema(hidden = true)
+    @JsonProperty("result_url")
     String resultUrl,
+
     @Schema(hidden = true)
+    @JsonProperty("success_redirect_url")
     String successRedirectUrl,
+
     @Schema(hidden = true)
+    @JsonProperty("error_redirect_url")
     String errorRedirectUrl,
+
+    @JsonProperty("card_id")
     String cardId,
+
+    @JsonProperty("is_installment")
     Integer isInstallment
 ) {}
